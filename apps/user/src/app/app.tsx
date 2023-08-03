bda798a5bb7cc1f7326e649e042db84221ee497c
Jaimin Parmar <jaimins100@gmail.com>
2023-08-03 20:28:19 +0530

    generate user app

---
+++ apps/user/src/app/app.tsx
@@ -0,0 +1,83 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome user 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+if (import.meta.vitest) {
+  // add tests related to your file here
+  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
+
+  const { it, expect, beforeEach } = import.meta.vitest;
+  let render: any;
+
+  beforeEach(async () => {
+    render = (await import('@testing-library/react')).render;
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+        {/* START: routes */}
+        {/* These routes and navigation have been generated for you */}
+        {/* Feel free to move and update them to fit your needs */}
+        <br />
+        <hr />
+        <br />
+        <div role="navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page-2">Page 2</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                This is the generated root route.{' '}
+                <Link to="/page-2">Click here for page 2.</Link>
+              </div>
+            }
+          />
+          <Route
+            path="/page-2"
+            element={
+              <div>
+                <Link to="/">Click here to go back to root page.</Link>
+              </div>
+            }
+          />
+        </Routes>
+        {/* END: routes */}
+      </BrowserRouter>
+    );
+    expect(getByText(/Welcome user/gi)).toBeTruthy();
+  });
+}
