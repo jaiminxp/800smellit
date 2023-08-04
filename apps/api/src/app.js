20458f2febfbb0af557dd81cc6a47c048fbca298
Jaimin Parmar <jaimins100@gmail.com>
2023-08-04 14:04:24 +0530

    fix log message

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -81,5 +81,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(port, () => {
-  console.log(`API is running at https://localhost:${port}`)
+  console.log(`API is running at http://localhost:${port}`)
 })
