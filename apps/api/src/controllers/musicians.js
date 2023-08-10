0922755ba10aa34131753c37566d7a14c9734dc5
Jaimin Parmar <jaimins100@gmail.com>
2023-08-10 18:18:07 +0530

    fix venue events not updating

--- apps/api/src/controllers/musicians.js
+++ apps/api/src/controllers/musicians.js
@@ -28,7 +28,7 @@ const createMusician = async (req, res) => {
   const eventList = []
 
   const createEvents = events.map(
-    async ({ date, time, venue, ...eventBody }) => {
+    async ({ date, time, venue: venueId, ...eventBody }) => {
       let eventDate = ''
 
       if (time) {
@@ -39,7 +39,7 @@ const createMusician = async (req, res) => {
 
       const event = new Event({
         ...eventBody,
-        venue: venue._id,
+        venue: venueId,
         date: eventDate,
       })
 
@@ -49,7 +49,7 @@ const createMusician = async (req, res) => {
         name: stray?.name || newMusician.name,
       }
 
-      await Venue.findByIdAndUpdate(venue._id, {
+      await Venue.findByIdAndUpdate(venueId, {
         $push: { events: event },
       })
 
