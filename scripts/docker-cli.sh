5b9b53a63402c762f674a2c24b4a322f9b72d017
Jaimin Parmar <jaimins100@gmail.com>
2023-12-13 18:21:41 +0530

    add commands
    - run container with volumes
    - display container logs

--- scripts/docker-cli.sh
+++ scripts/docker-cli.sh
@@ -4,8 +4,20 @@ docker build -t 800smellit-image .
 # run the container
 docker run -p 3000:3000 -d --name 800smellit 800smellit-image
 
+# run the container with bindmount volume
+docker run -v $(pwd)/apps:/app/apps -p 3000:3000 -d --name 800smellit 800smellit-image
+
+# run the container with anonymous volume for node_modules
+docker run -v $(pwd)/apps:/app/apps -v /app/node_modules -p 3000:3000 -d --name 800smellit 800smellit-image
+
+# run the container with readonly bindmount volume (:ro flag)
+docker run -v $(pwd)/apps:/app/apps:ro -v /app/node_modules -p 3000:3000 -d --name 800smellit 800smellit-image
+
 # kill and remove the container (forced)
 docker rm 800smellit -f
 
 # access container cli
 docker exec -it 800smellit bash
+
+# display container logs
+docker logs 800smellit
